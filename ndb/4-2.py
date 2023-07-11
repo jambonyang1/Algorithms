61de9508d6f32d89a281aff5ea5b4030aa7546d6
jambonyang1 <1106wjdgus@gmail.com>
2023-07-11 20:25:43 +0900

    4-2 답 추가

--- ndb/4-2.py
+++ ndb/4-2.py
@@ -1,16 +1,34 @@
 N = int(input())
-N += 1
 answer = 0
-if N >= 23:
-    answer += 3600 * 3
-    N -= 3
-elif N >= 13:
-    answer += 3600 * 2
-    N -= 2
-elif N >= 3:
-    answer += 3600
-    N -= 1
+for h in range(N+1):
+    for m in range(60):
+        for s in range(60):
+            if '3' in f'{h}{m}{s}':
+                answer += 1
 
-answer += N * 1575
+print(answer)
 
-print(answer, N)
+
+
+
+
+
+
+
+
+
+# N += 1
+# answer = 0
+# if N >= 23:
+#     answer += 3600 * 3
+#     N -= 3
+# elif N >= 13:
+#     answer += 3600 * 2
+#     N -= 2
+# elif N >= 3:
+#     answer += 3600
+#     N -= 1
+
+# answer += N * 1575
+
+# print(answer, N)
