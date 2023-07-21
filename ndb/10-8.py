a7e226e38631b38c23593e14409a6247b3a7353c
jambonyang1 <1106wjdgus@gmail.com>
2023-07-21 18:48:05 +0900

    크루스칼 알고리즘 문제 - 도시 분할 계획

---
+++ ndb/10-8.py
@@ -0,0 +1,34 @@
+def find(parent, x):
+    if x != parent[x]:
+        parent[x] = find(parent, parent[x])
+    return parent[x]
+
+def union(parent, a, b):
+    a = find(parent, a)
+    b = find(parent, b)
+
+    if a < b:
+        parent[b] = a
+    elif a > b:
+        parent[a] = b
+
+N, M = map(int, input().split())
+parent = [i for i  in range(N+1)]
+edges = []
+result = 0
+last = 0
+
+for _ in range(M):
+    a, b, cost = map(int, input().split())
+    edges.append((cost, a, b))
+
+edges.sort()
+
+for edge in edges:
+    cost, a, b = edge
+    if find(parent, a) != find(parent, b):
+        union(parent, a, b)
+        result += cost
+        last = cost
+
+print(result - last)
