7fe4fee65fa55c49290d07e419237fa765b1a9bf
jambonyang1 <1106wjdgus@gmail.com>
2023-10-22 20:06:52 +0900

    감소수열

---
+++ 11722.py
@@ -0,0 +1,10 @@
+N = int(input())
+array = list(map(int, input().split()))
+
+dp = [1] * N
+for i in range(0, N-1):
+    for j in range(i+1, N):
+        if array[i] > array[j]:
+            dp[j] = max(dp[j], dp[i]+1)
+
+print(max(dp))
