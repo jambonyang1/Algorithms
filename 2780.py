429471589c592ea9b8fd18f17cb5bfc36610b03d
jambonyang1 <1106wjdgus@gmail.com>
2023-11-02 09:11:08 +0900

    내가 넣는 값이 더 적도록 수정

--- 2780.py
+++ 2780.py
@@ -1,7 +1,7 @@
 import sys
 input = sys.stdin.readline
 
-values = [[1, 1], [1, 2], [1, 3], [1, 2], [1, 3], [1, 4], [1, 3], [1, 3], [1, 3], [1, 2]]
+values = [[1] for _ in range(10)]
 dial = [[7], [2, 4], [1, 3, 5], [2, 6], [1, 5, 7], [2, 4, 6, 8], [3, 5, 9], [0, 4, 8], [5, 7, 9], [6, 8]]
 
 T = int(input())
@@ -10,11 +10,11 @@ for _ in range(T):
     cases.append(int(input()))
 
 mx = max(cases)
-for a in range(mx-2):
+for a in range(mx-1):
     for i in range(10):
         tmp = 0
         for j in dial[i]:
-            tmp = (tmp + values[j][a+1]) % 1234567
+            tmp = (tmp + values[j][a]) % 1234567
         values[i].append(tmp)
 
 results = [0] * mx
