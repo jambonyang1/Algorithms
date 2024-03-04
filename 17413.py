bf5e7dbcbdfa84cb331e232394e276e9c7217342
jambonyang1 <1106wjdgus@gmail.com>
2024-03-04 12:11:40 +0900

    재풀이

--- 17413.py
+++ 17413.py
@@ -1,26 +1,52 @@
 S = input()
 
+inTag = False
 answer = ""
 word = ""
-isTag = False
-
-for ch in S:
-    if ch == "<":
-        answer += word[::-1] + ch
-        word = ""
-        isTag = True
-    elif ch == ">":
-        answer += ch
-        isTag = False
-    elif ch == " ":
-        if not isTag:
-            answer += word[::-1]
-            word = ""
-        answer += ch
+for c in S:
+    if inTag:
+        answer += c
+        if c == ">":
+            inTag = False
     else:
-        if isTag:
-            answer += ch
+        if c == "<":
+            inTag = True
+            answer += word[::-1] + c
+            word = ""
+        elif c == " ":
+            answer += word[::-1] + " "
+            word = ""
         else:
-            word += ch
+            word += c
 answer += word[::-1]
 print(answer)
+
+
+
+
+# S = input()
+
+# answer = ""
+# word = ""
+# isTag = False
+
+# for ch in S:
+#     if ch == "<":
+#         answer += word[::-1] + ch
+#         word = ""
+#         isTag = True
+#     elif ch == ">":
+#         answer += ch
+#         isTag = False
+#     elif ch == " ":
+#         if not isTag:
+#             answer += word[::-1]
+#             word = ""
+#         answer += ch
+#     else:
+#         if isTag:
+#             answer += ch
+#         else:
+#             word += ch
+# answer += word[::-1]
+# print(answer)
