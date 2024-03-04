0ef8fbcf4eaa6b3a2aa9f9d8e995ce484dfa2db4
jambonyang1 <1106wjdgus@gmail.com>
2024-03-04 17:43:40 +0900

    재풀이, 층별로 물 고인 양을 측정함

--- 14719.py
+++ 14719.py
@@ -1,24 +1,42 @@
 H, W = map(int,input().split())
 blocks = list(map(int,input().split()))
 
-stack = []
-result = 0
-
-while True:
-    for w in range(len(stack)):
-        bl = blocks[w]
-        if len(stack) == 0:
-            stack.append(bl)
-            continue
-        if bl >= stack[0]:
-            for i in stack:
-                result += stack[0] - i
-            stack = [bl]
+answer = 0
+for h in range(1, H+1):
+    water = 0
+    isWall = False
+    for b in blocks:
+        if b >= h:
+            if isWall:
+                answer += water
+            else:
+                isWall = True
+            water = 0
         else:
-            stack.append(bl)
-    blocks = list(reversed(stack))
-    stack = []
-    if len(blocks) == 0:
-        break
+            if isWall:
+                water += 1
 
-print(result)
+print(answer)
+
+
+# stack = []
+# result = 0
+
+# while True:
+#     for w in range(len(stack)):
+#         bl = blocks[w]
+#         if len(stack) == 0:
+#             stack.append(bl)
+#             continue
+#         if bl >= stack[0]:
+#             for i in stack:
+#                 result += stack[0] - i
+#             stack = [bl]
+#         else:
+#             stack.append(bl)
+#     blocks = list(reversed(stack))
+#     stack = []
+#     if len(blocks) == 0:
+#         break
+
+# print(result)
