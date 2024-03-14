d32ea2fdd47e39ea9e67d0a1840c454c4e84b209
jambonyang1 <1106wjdgus@gmail.com>
2024-03-14 16:22:41 +0900

    스택 dfs 풀이 추가

--- 2606.py
+++ 2606.py
@@ -1,17 +1,36 @@
 N = int(input())
 E = int(input())
+
 graph = [[] for _ in range(N+1)]
 for _ in range(E):
     a, b = map(int, input().split())
     graph[a].append(b)
     graph[b].append(a)
 
+# 스택을 사용한 dfs
+def dfs_stack():
+    stack = [1]
+    visited = []
+
+    while stack:
+        cur = stack.pop()
+        if cur not in visited:
+            visited.append(cur)
+            for node in graph[cur]:
+                if node not in visited:
+                    stack.append(node)
+    
+    return len(visited) - 1
+
+print(dfs_stack())
+
+# 재귀 함수를 이용한 dfs
 visited = [False] * (N+1)
-def dfs(v):
+def dfs_recursive(v):
     visited[v] = True
     for i in graph[v]:
         if not visited[i]:
-            dfs(i)
+            dfs_recursive(i)
 
-dfs(1)
+dfs_recursive(1)
 print(visited.count(True) - 1)
