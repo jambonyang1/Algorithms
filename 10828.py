9dbc02156316687bd50104a00b314b3f57b074b6
jambonyang1 <1106wjdgus@gmail.com>
2024-07-10 16:10:35 +0900

    좀 더 깔끔하게 수정

--- 10828.py
+++ 10828.py
@@ -1,28 +1,23 @@
 import sys
+
 input = sys.stdin.readline
 
 N = int(input())
 stack = []
 
 for _ in range(N):
-    command = list(input().split())
-    c = command[0]
-    if c == 'push':
-        stack.append(command[1])
-    elif c == 'pop':
+    tmp = list(input().split())
+    order = tmp[0]
+    if order == "push":
+        stack.append(tmp[1])
+    elif order == "pop":
         if len(stack) == 0:
             print(-1)
         else:
             print(stack.pop())
-    elif c == 'size':
+    elif order == "size":
         print(len(stack))
-    elif c == 'empty':
-        if len(stack) == 0:
-            print(1)
-        else:
-            print(0)
-    elif c == 'top':
-        if len(stack) == 0:
-            print(-1)
-        else:
-            print(stack[-1])
+    elif order == "empty":
+        print(1 if len(stack) == 0 else 0)
+    elif order == "top":
+        print(-1 if len(stack) == 0 else stack[-1])
