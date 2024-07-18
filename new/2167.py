0c4178eca8151dbcf3a3847439a06e83510528dc
jambonyang1 <1106wjdgus@gmail.com>
2024-07-18 22:05:04 +0900

    이전 문제 더 빠른 버전. 미리 합을 계산해 놓아 매번 수십 수백번의 연산의 중복을 방지

--- new/2167.py
+++ new/2167.py
@@ -4,11 +4,16 @@ input = sys.stdin.readline
 
 N, M = map(int, input().split())
 matrix = [list(map(int, input().split())) for _ in range(N)]
+for i in range(N):
+    for j in reversed(range(M)):
+        matrix[i][j] = sum(matrix[i][: j + 1])
 K = int(input())
 for _ in range(K):
     i, j, x, y = map(int, input().split())
     sum = 0
     for p in range(i - 1, x):
-        for q in range(j - 1, y):
-            sum += matrix[p][q]
+        if j == 1:
+            sum += matrix[p][y - 1]
+        else:
+            sum += matrix[p][y - 1] - matrix[p][j - 2]
     print(sum)
