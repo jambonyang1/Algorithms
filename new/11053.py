ccd63f9bd4c0a550b98db540edb394e3f43ac882
jambonyang1 <1106wjdgus@gmail.com>
2024-07-27 19:04:07 +0900

    가장 긴 증가하는 부분수열

---
+++ new/11053.py
@@ -0,0 +1,12 @@
+A = int(input())
+li = list(map(int, input().split()))
+
+dp = [1]
+for i in range(1, A):
+    tmp = [1]
+    for j in range(i):
+        if li[j] < li[i]:
+            tmp.append(dp[j] + 1)
+    dp.append(max(tmp))
+
+print(max(dp))
