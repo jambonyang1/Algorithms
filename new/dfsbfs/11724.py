5c6d64f0003d633f913247e129b2ba2138ac71aa
jambonyang1 <1106wjdgus@gmail.com>
2024-07-29 16:34:18 +0900

    dfs

---
+++ new/dfsbfs/11724.py
@@ -0,0 +1,22 @@
+N, M = map(int, input().split())
+graph = [[] for i in range(N + 1)]
+for _ in range(M):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+visited = [False] * (N + 1)
+ans = 0
+for i in range(1, N + 1):
+    if visited[i]:
+        continue
+    ans += 1
+    stack = [i]
+    while stack:
+        now = stack.pop()
+        visited[now] = True
+        for v in graph[now]:
+            if not visited[v]:
+                stack.append(v)
+
+print(ans)
