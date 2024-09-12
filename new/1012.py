20b44d9dff16ca85d1876312dd08d4c121711bf0
jambonyang1 <1106wjdgus@gmail.com>
2024-09-12 15:12:50 +0900

    다시 풀어봄. 풀이는 똑같음

--- new/1012.py
+++ new/1012.py
@@ -10,12 +10,13 @@ for _ in range(T):
     M, N, K = map(int, input().split())
     table = [[False] * M for _ in range(N)]
     for _ in range(K):
-        x, y = map(int, input().split())
-        table[y][x] = True
+        m, n = map(int, input().split())
+        table[n][m] = True
     cnt = 0
     for i in range(N):
         for j in range(M):
             if table[i][j]:
+                cnt += 1
                 stack = [[i, j]]
                 while stack:
                     r, c = stack.pop()
@@ -25,5 +26,26 @@ for _ in range(T):
                         if 0 <= nr < N and 0 <= nc < M:
                             if table[nr][nc]:
                                 stack.append([nr, nc])
-                cnt += 1
     print(cnt)
+
+# for _ in range(T):
+#     M, N, K = map(int, input().split())
+#     table = [[False] * M for _ in range(N)]
+#     for _ in range(K):
+#         x, y = map(int, input().split())
+#         table[y][x] = True
+#     cnt = 0
+#     for i in range(N):
+#         for j in range(M):
+#             if table[i][j]:
+#                 stack = [[i, j]]
+#                 while stack:
+#                     r, c = stack.pop()
+#                     table[r][c] = False
+#                     for dr, dc in d:
+#                         nr, nc = r + dr, c + dc
+#                         if 0 <= nr < N and 0 <= nc < M:
+#                             if table[nr][nc]:
+#                                 stack.append([nr, nc])
+#                 cnt += 1
+#     print(cnt)
